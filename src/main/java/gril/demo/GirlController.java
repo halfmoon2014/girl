1f8e412dfd23fa4e866a15ab92a24c8611ad5748
halfmoon2014 <mrzhangmh@gmail.com>
2017-11-06 11:14:03 +0800

    l

--- src/main/java/gril/demo/GirlController.java
+++ src/main/java/gril/demo/GirlController.java
@@ -1,8 +1,8 @@
 package gril.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
 public class GirlController {
@@ -12,4 +12,23 @@ public class GirlController {
     public List<Girl> girlsList(){
         return girlRepository.findAll();
     }
+
+    @PostMapping(value = "/girls")
+    public  Girl girlAdd(@RequestParam("cupSize") String cupSize,
+                          @RequestParam("age") Integer age){
+        Girl girl= new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        return girlRepository.save(girl);
+    }
+
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id){
+        return  girlRepository.getOne(id);
+    }
+    //更新
+    //@PutMapping(value = "/girls/{id}")
+
+    //删除
+    //@DeleteMapping(value = "/girls/{id}")
 }
